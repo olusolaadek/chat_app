1a8ec1ed15117f9e25b1cbfd7b151077085df3fb
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2020-07-31 00:42:08 +0100

    add a stateful widget ChatHead

---
+++ lib/ChatHead.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatHead extends StatefulWidget {
+  @override
+  _ChatHeadState createState() => _ChatHeadState();
+}
+
+class _ChatHeadState extends State<ChatHead> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Chat Head");
+  }
+}
