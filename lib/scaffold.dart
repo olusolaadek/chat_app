0f49794778e0c84f17df2241f6c5d1461cb9d9f5
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2020-07-31 00:37:41 +0100

    change body widget to ListView

---
+++ lib/scaffold.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyChatApp extends StatefulWidget {
+  @override
+  _MyChatAppState createState() => _MyChatAppState();
+}
+
+class _MyChatAppState extends State<MyChatApp> {
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    // return Container({Text('Text here')});
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.help),
+              onPressed: () {
+                print('Button was pressed');
+              },
+            ),
+          ],
+        ),
+        body: ListView(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.mail_outline), title: Text("Chats")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), title: Text("Settings")),
+          ],
+          onTap: (int index) => {
+            print('Index is $index'),
+            setState(() {
+              _currentIndex = index;
+            }),
+            //setState(())
+            // _currentIndex = index
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('FAB was pressed');
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
